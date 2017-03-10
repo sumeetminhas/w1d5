f97e0a0cf36d460da039c557e16889e4c54bd649
Sumeet <sumeetm273@gmail.com>
2017-03-10 20:05:10 +0000

    module exports with private function

---
+++ module.exports.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sum: 5,
+  add: function () {
+    console.log("hi" + this.sum);
+    console.log(hi);
+  }
+};
+function hi() {
+  return okay;
+}
---
+++ node.js
@@ -0,0 +1,5 @@
+var greeting = require("./module.exports");
+
+console.log(greeting.sum);
+
+greeting.add();
